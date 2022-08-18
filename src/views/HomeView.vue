07f366759518d7b3d8363dabbef280aa29261547
Zafar <rafazafar@gmail.com>
2022-08-18 12:02:37 +0900

    updated HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="container">
+    <PhotoSearch />
+    <PhotoGallery />
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, onMounted } from "vue"
+import PhotoSearch from "@/components/PhotoSearch.vue"
+import PhotoGallery from "@/components/PhotoGallery.vue"
+
+export default defineComponent({
+  components: {
+    PhotoSearch,
+    PhotoGallery,
+  },
+})
+</script>
