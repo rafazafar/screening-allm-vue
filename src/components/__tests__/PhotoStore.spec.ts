3508eef83389c306fee496dff233f623fe28b24c
Zafar <rafazafar@gmail.com>
2022-08-18 15:49:55 +0900

    add PhotosStore tests

---
+++ src/components/__tests__/PhotoStore.spec.ts
@@ -0,0 +1,19 @@
+import { it, describe, expect, beforeEach } from "vitest"
+import { setActivePinia, createPinia } from "pinia"
+import { usePhotosStore } from "../../stores/photos"
+
+describe("Photos Store", () => {
+  describe("Initial State", () => {
+    beforeEach(() => {
+      setActivePinia(createPinia())
+    })
+    it("empty photos array", () => {
+      const photosStore = usePhotosStore()
+      expect(photosStore.photos.length).toBe(0)
+    })
+    it("empty tags", () => {
+      const photosStore = usePhotosStore()
+      expect(photosStore.tag).toBe("")
+    })
+  })
+})
