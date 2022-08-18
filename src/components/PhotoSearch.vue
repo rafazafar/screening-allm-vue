da98c6f764f9979950744a77daeae12d66958239
Zafar <rafazafar@gmail.com>
2022-08-18 12:01:13 +0900

    add PhotoSearch component for seaching by tags

---
+++ src/components/PhotoSearch.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mx-auto px-5 lg:pt-12 lg:px-32">
+    <input
+      type="text"
+      v-model="inputTag"
+      placeholder="Search By Tag"
+      class="input input-bordered w-full max-w-xs"
+    /><button @click="searchByTag" class="btn ml-5">Search</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+import { usePhotosStore } from "@/stores/photos"
+
+export default defineComponent({
+  setup() {
+    const photosStore = usePhotosStore()
+    const inputTag = ref("")
+
+    function searchByTag(): void {
+      photosStore.searchByTag(inputTag.value)
+    }
+
+    return { searchByTag, inputTag }
+  },
+})
+</script>
